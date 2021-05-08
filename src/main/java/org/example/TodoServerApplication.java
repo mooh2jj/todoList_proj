4048728c55f192a305d268678deb2b4def2cd134
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-05-08 13:56:12 +0900

    서비스 구현

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.repository.TodoRepository;
+
 public class TodoServerApplication {
     public static void main(String[] args) {
         System.out.println("Hello todo");
+        TodoRepository repository;
+//        repository.
     }
 }
